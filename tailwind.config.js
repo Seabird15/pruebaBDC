6e5a7270f58e5c8387ab25be3c7c43f2bcc327b3
Daniela Lizama <2b-0005@MacBook-Pro-de-IT.local>
2023-04-06 15:41:20 -0400

    cards

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'nunito-sans': ['Nunito Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
